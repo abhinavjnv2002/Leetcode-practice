7c35c93e9dd600f7ebb4a9c6d14b1545cba9f1e2
abhinavjnv2002 <abhinavjnv2002@gmail.com>
2024-07-12 04:26:03 +0530

    maxiProfitSBuySellStock

---
+++ Arrays/MaxiProfitStock.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int maxProfit(int[] prices) {        
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        
+        return maxProfit;
+    }
+}
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        int min=Integer.MAX_VALUE;
+
+        for(int i=0;i<prices.length;i++){
+            min=Math.min(min,prices[i]);
+            max=Math.max(max,prices[i]-min);
+        }
+        return max;
+    }
+} {
+    
+}
